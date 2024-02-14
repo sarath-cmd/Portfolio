c8dd121d421e2ba4def0d3b519e400efab8997ea
sarath-cmd <sarathredmagic@gmail.com>
2024-02-14 09:57:28 +0530

    all base file imported

---
+++ index.js
@@ -0,0 +1,12 @@
+function menuClick()
+{
+    var menu = document.getElementById('mobileMenu');
+    if(menu.classList.contains('hidden'))
+    {
+        menu.classList.remove('hidden');
+    }
+    else
+    {
+        menu.classList.add('hidden');
+    }
+}
